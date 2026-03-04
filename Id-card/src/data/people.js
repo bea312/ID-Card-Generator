5969891fa0ed397659cea1178e10f1353ac06446
bea312 <mukagasirabob@gmail.com>
2026-03-04 13:19:30 -0800

    adding peoplejs

---
+++ Id-card/src/data/people.js
@@ -0,0 +1,25 @@
+import beatricePhoto from "../assets/Mukagasirabo Beatrice _photo.JPG";
+
+export const people = [
+  {
+    id: "REG-1501",
+    name: "Mukagasirabo Beatrice",
+    role: "Frontend Developer",
+    department: "Frontend ",
+    avatar: beatricePhoto,
+  },
+  {
+    id: "REG-1052",
+    name: "Uwase Alice",
+    role: "Backend Developer",
+    department: "Backend ",
+    avatar: beatricePhoto,
+  },
+  {
+    id: "REG-1503",
+    name: "Quincy Ada",
+    role: "Data Analyst",
+    department: "Full Stack",
+    avatar: beatricePhoto,
+  },
+];
